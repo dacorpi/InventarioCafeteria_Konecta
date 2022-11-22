d8130f261fc512d774d6424d29860bcb5c37580b
dacorpi <davidcordobapimienta1@gmail.com>
2022-11-21 20:39:08 -0500

    first commit

---
+++ Template/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <title>Cafetería Konecta</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container-fluid bg-info">
+        <div class="row">
+            <div class="col-md">   
+                <header class="py-3">
+                    <h3 class="text-center">CAFETERÍA KONECTA</h3>
+                </header>
+            </div>
+        </div>
+    </div>
+</body>
